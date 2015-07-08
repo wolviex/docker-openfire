b4857ff7fcd9fbbf5bcd40b6e27d78757b9986ff
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-08 08:41:15 -0400

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+/usr/bin/mysqld_safe &
+ sleep 10s
+
+ mysqladmin -u root password mysqlpsswd
+ mysqladmin -u root -pmysqlpsswd reload
+ mysqladmin -u root -pmysqlpsswd create  openfire
+
+echo "GRANT ALL ON openfire.* TO openfireuser@localhost IDENTIFIED BY 'openfiredbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
+
+wget http://www.igniterealtime.org/downloads/download-landing.jsp?file=openfire/openfire_3.10.2_all.deb
+mv downloads/download-landing.jsp?file=openfire/openfire_3.10.2_all.deb openfire_3.10.2_all.deb
+dpkg -i openfire_3.10.2_all.deb
+
+ #to clear some data before saving this layer ...a docker image
+ apt-get clean
+ rm -rf /tmp/* /var/tmp/*
+ rm -rf /var/lib/apt/lists/*
+ 
+killall mysqld
+sleep 10s
