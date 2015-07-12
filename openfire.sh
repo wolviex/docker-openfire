4e049d6d5ecdc8b990ed161d366411ea2e437775
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-12 11:59:23 -0400

    Update openfire.sh

--- openfire.sh
+++ openfire.sh
@@ -8,16 +8,7 @@ if [ -f /etc/default/openfire ] ; then
         . /etc/default/openfire
 fi
 
-#/usr/
-if [ -z $JAVA_HOME ]; then
- JAVA_HOME=$(LC_ALL=C update-alternatives --display java \
-    | grep best \
-    | grep -oe \/.*\/bin\/java \
-    | sed 's/\/bin\/java//g')
-    echo "best java alternative in: "$JAVA_HOME
-fi
-
-
+JAVA_HOME=/usr
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:${JAVA_HOME}/bin
 JAVA=${JAVA_HOME}/bin/java
 NAME=openfire
@@ -25,11 +16,9 @@ DESC=openfire
 DAEMON_DIR=/usr/share/openfire
 DAEMON_LIB=${DAEMON_DIR}/lib
 
-DAEMON_OPTS="$DAEMON_OPTS -server -DopenfireHome=${DAEMON_DIR} \
+DAEMON_OPTS="-server -DopenfireHome=${DAEMON_DIR} \
  -Dopenfire.lib.dir=${DAEMON_LIB} -classpath ${DAEMON_LIB}/startup.jar\
  -jar ${DAEMON_LIB}/startup.jar"
 
 
-#exec start-stop-daemon --start --quiet --background --make-pidfile \
-                --pidfile /var/run/$NAME.pid --chuid openfire:openfire \
-                --exec $JAVA -- $DAEMON_OPTS
+exec $JAVA $DAEMON_OPTS
