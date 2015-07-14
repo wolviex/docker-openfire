0e531ff77cb8ffdb2f56b54d468b624c866e2cd7
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-13 23:26:36 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -16,11 +16,13 @@ dpkg -i openfire_3.10.2_all.deb
 rm openfire_3.10.2_all.deb
 
 #installing sparkweb
-wget http://www.igniterealtime.org/downloads/download-landing.jsp?file=sparkweb/sparkweb_0_9_0.tar.gz
-mv download-landing.jsp\?file\=sparkweb%2Fsparkweb_0_9_0.tar.gz sparkweb_0_9_0.tar.gz
-
+wget http://www.igniterealtime.org/downloadServlet?filename=sparkweb/sparkweb_0_9_0.tar.gz
+mv downloadServlet?filename=sparkweb%2Fsparkweb_0_9_0.tar.gz sparkweb_0_9_0.tar.gz
+tar -xvf sparkweb_0_9_0.tar.gz
 rm sparkweb_0_9_0.tar.gz
-
+mv sparkweb /usr/share/openfire/plugins/admin/webapp/
+mv /usr/share/openfire/plugins/admin/webapp/sparkweb/SparkWeb.html  /usr/share/openfire/plugins/admin/webapp/sparkweb/index.html
+ 
  #to clear some data before saving this layer ...a docker image
  apt-get clean
  rm -rf /tmp/* /var/tmp/*
