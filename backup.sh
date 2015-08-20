bd9faa494ac95cc7af450b67d2de9bb83e804fa6
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-20 00:03:09 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Backup mysql
-mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
-
 #Backup important file ... of the configuration ...
 cp  /etc/hosts  /var/backups/
 
