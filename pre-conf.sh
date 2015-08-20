3cfa7c30e0df80638d29cef445164a0817f78e92
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-20 00:01:38 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-/usr/bin/mysqld_safe &
- sleep 10s
-
- mysqladmin -u root password mysqlpsswd
- mysqladmin -u root -pmysqlpsswd reload
- mysqladmin -u root -pmysqlpsswd create  openfire
-
-echo "GRANT ALL ON openfire.* TO openfireuser@localhost IDENTIFIED BY 'openfiredbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
-
 #installing openfire
 wget http://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_3.10.2_all.deb
 mv downloadServlet?filename=openfire%2Fopenfire_3.10.2_all.deb openfire_3.10.2_all.deb
@@ -27,6 +18,3 @@ mv /usr/share/openfire/plugins/admin/webapp/sparkweb/SparkWeb.html  /usr/share/o
  apt-get clean
  rm -rf /tmp/* /var/tmp/*
  rm -rf /var/lib/apt/lists/*
- 
-killall mysqld
-sleep 10s
