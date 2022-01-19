cf23879ec62d0b0952ad8a3b473dc54c98dd9c8c
Joe Manifold <jrmanifold@gmail.com>
2022-01-19 14:13:19 -0800

    Openfire 4.7.0

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,7 +2,7 @@
 set -e
 
 #installing openfire
-wget -O openfire.deb http://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4.6.7_all.deb
+wget -O openfire.deb http://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4.7.0_all.deb
 dpkg -i openfire.deb
 rm openfire.deb
 
